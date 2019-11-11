f18687c586f8b178d10942b49051c0c6f71d9e19
welljon <jwells@mylifedigital.co.uk>
2019-11-11 08:07:22 +0000

    First branching text adventure."

--- episode-05/textadventure.go
+++ episode-05/textadventure.go
@@ -67,4 +67,36 @@ func (node *storyNode) play() {
 func main() {
 	scanner = bufio.NewScanner(os.Stdin)
 
+	start := storyNode{text: `
+	You wake up in a dark cave.
+	You see see that there are 3 passages leading out.
+	The north passage leads into darkness
+	To the south, a passage appears to head upwards.
+	The eastern passage appears flat, but has a disturbing smell coming from it.`}
+
+	darkRoom := storyNode{text: "It is pitch black. You cannot see a thing."}
+
+	darkRoomLit := storyNode{text: "The dark passage is now lit by your torch. You can continue north or head back south"}
+
+	grue := storyNode{text: "Whilst stumbling around in the darkness you are eaten by a grue"}
+
+	trap := storyNode{text: "You trigger a hidden trap and fall to your doom"}
+
+	treasure := storyNode{text: "You arrive at a small chamber, filled with wonderful treasure!"}
+
+	start.addChoice("N", "Go North", &darkRoom)
+	start.addChoice("S", "Go South", &darkRoom)
+	start.addChoice("E", "Go East", &trap)
+
+	darkRoom.addChoice("S", "Try to go back south", &grue)
+	darkRoom.addChoice("L", "Light up Lantern", &darkRoomLit)
+
+	darkRoomLit.addChoice("N", "Go North", &treasure)
+	darkRoomLit.addChoice("S", "Try to go back south", &start)
+
+	start.play()
+
+	fmt.Println()
+	fmt.Println("The End.")
+
 }
